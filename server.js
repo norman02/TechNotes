4c5adc7b2bc3e0add2bb255c96e0bc884c2f534d
John N. Norman <normanj777@gmail.com>
2023-01-09 10:10:35 -0500

    secure cors

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const {logger} = require('./middleware/logger')
-const PORT = process.env.PORT || 3500;
+const express = require('express')
+const app = express()
+const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const PORT = process.env.PORT || 3500
 
 app.use(logger)
 
+app.use(cors(corsOptions))
+
 app.use(express.json())
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use(cookieParser())
 
-app.use("/", require("./routes/root"));
+app.use('/', express.static(path.join(__dirname, 'public')))
 
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) res.json({ message: "404 Not Found" });
-  else {
-    res.type("txt").send("404 Page not found");
-  }
-});
+app.use('/', require('./routes/root'))
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
